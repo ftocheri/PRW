2b4af9b330fc53c5e5f5cec3fdf4e8d48539272a
Forrest Tocheri <fstocheri@student.fullsail.edu>
2018-11-25 16:20:17 -0700

    validation errors

--- PRW_Wk1/ES2018 CRUD Project/dist/main.js
+++ PRW_Wk1/ES2018 CRUD Project/dist/main.js
@@ -1,12 +1,19 @@
 'use strict';
 
+//create the array to hold for local storage
 var articleList = [];
+//check if the local storage have values already
 if (JSON.parse(localStorage.getItem('article')) != null) {
+    //if it does, save the variable to the storage values
     articleList = JSON.parse(localStorage.getItem('article'));
 }
+//function to run on the window load
 window.onload = function () {
+    //save the localstorage data to a new variable
     var data = JSON.parse(this.localStorage.getItem('article'));
+    //for each item within my storage
     $.each(data, function (i, item) {
+        //create the element that will show the saved recipe
         var myList = $('#myList');
         imgNum = Math.floor(Math.random() * 9);
         img = '<img src="images/' + imgNum + '.jpg" alt="Recipe Image">';
@@ -15,6 +22,7 @@ window.onload = function () {
     });
 };
 
+//fetch function for the data stored in my JSON file
 fetch('https://api.myjson.com/bins/gg3eh').then(function (response) {
     if (response.ok) {
         return response.json();
@@ -35,6 +43,7 @@ fetch('https://api.myjson.com/bins/gg3eh').then(function (response) {
     return console.log('error is', error);
 });
 
+//function for the changing of my star rating system
 $(document).ready(function () {
     // Check Radio-box
     $(".rating input:radio").attr("checked", false);
@@ -44,6 +53,9 @@ $(document).ready(function () {
         $(this).parent().addClass('checked');
     });
 });
+//function to save data from my form
+//this will save the item to my localstorage
+//& create a new article element with the formatted data
 function myList() {
     var r = void 0;
     var radios = document.getElementsByName('rating');
@@ -63,20 +75,25 @@ function myList() {
     console.log(arrayLength);
     $("#myList").append("<article id=" + arrayLength + "><h4>" + img + $("#rName").val() + "</h4><p>" + $("#rDesc").val() + "</p><p>Category: " + $("#rCat").val() + "</p><p>Star Rating: " + r + "</p><p class='remove' onclick='deleteMe(this);'>" + "<span class='fa fa-trash'></span>" + "</p></article>");
 
+    //section that will change my localstorage
     var entry = {};
     entry.id = arrayLength;
     entry.name = $("#rName").val();
     entry.desc = $("#rDesc").val();
     entry.cat = $("#rCat").val();
     entry.rate = r;
+    //add the new item to my array
     articleList.push(entry);
-    console.log(entry);
+    //console.log(entry);
+    //add that array list to the localstorage
     localStorage.setItem('article', JSON.stringify(articleList));
 }
+//function to clear the form
 function formClear() {
     $("#rName").val("");
     $("#rDesc").val("");
     $("#rCat").val("");
+    //seperate area to control my radio buttons
     var radios = document.getElementsByName('rating');
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
@@ -105,12 +122,13 @@ function assignUpdate() {
         $("#rName").focus();
     }
 }
-
+//function for displaying the custom error message
 function displayError(id) {
     document.getElementById(id).style.display = "block";
     document.getElementById(id).innerHTML = document.getElementById(id).getAttribute("name") + " can not be left blank.";
     return false;
 }
+//function to clear the errormessage when it needs to be cleared
 function clearError() {
     document.getElementById('nMessage').style.display = "none";
     document.getElementById('dMessage').style.display = "none";
@@ -118,6 +136,8 @@ function clearError() {
     document.getElementById('rMessage').style.display = "none";
 }
 
+//function to delete the item from the page
+//also deletes custom items from the local storage
 function deleteMe(entry) {
     $(entry).parents("article").remove();
     //console.log($(entry).parents('article').attr("id"));
